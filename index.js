1a34e8f53d28096ddb5a8b3218899a16579746a6
Carrascoj1998 <carrascoj1998@gmail.com>
2022-06-22 21:51:00 -0500

    updated JS

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-let specChar = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '/', ';', ':', '.', '<', '>', '?', '@', '[', ']', '^', '_', '{', '}', '|', '~'];
-console.log(specChar);
-let numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-console.log(numberChar);
-let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-console.log(upperCase);
-let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-console.log(lowerCase);
+// Assignment Of Vars 
+var  specChar = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '/', ';', ':', '.', '<', '>', '?', '@', '[', ']', '^', '_', '{', '}', '|', '~'];
+
+var  numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var  upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var userInput =[];
 
 
-let userInput = []
+
+
+//Making Function for on Click 
 document.getElementById("generate").onclick = function () {
-    prompt("How many Characters would you like you passcode? ");
-    if (userInput < 8){
-        alert('must be over 8 Characters');
-    }
+     var userInput = prompt("How many Characters would you like you passcode? ");
+        if (userInput < 8 || userInput > 120){
+            alert("must be longer than 8 and less than 120 characters");
+            return;
+        }
 
-    else if (userInput < 120){
-        alert("must be under 120 Characters");
-    }
-    else{
 
-    }
+
+    
+
+     var specChar = confirm(" Would You Like Special Charcaters?");
+        if (specChar === true){
+            
+        }
+
+
+     
+     var lowerCase = confirm("Do You Want Lower Case Letters?");
+        if (lowerCase === true ){
+
+        }
+
+
+
+
+     var upperCase = confirm("Would You Like Upper Case Letters?");
+
+        if (upperCase === true ){
+
+        }
+
+     
+
 }
+
+

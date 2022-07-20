e18190bdc002a5b0c789c464d0a839e1df31238f
Carrascoj1998 <carrascoj1998@gmail.com>
2022-07-19 21:57:38 -0500

    updated Javascrpit

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 //Select button 
 var generateBtn = document.querySelector("#generate");
 
-var  numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//Setting Up Vars 
 
-var  upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var specChar = [];
+
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var userInput = [];
 
 
+
+
+
+// Making Function For Password 
 function generatePassword() {
     console.log("Button has been used");
 
@@ -20,36 +28,50 @@ function generatePassword() {
         return null;
         
     }
-    else {
-        (userInputQ)
+
+    else if (userInputQ > 8 || userInputQ < 120){
+        console.log(userInputQ);
     }
 
-    var specCharQ = confirm(" Would You Like Special Charcaters?");
-    if (specCharQ){
-        (specCharQ);
-    }
+
     else {
-        return null;
+        return null; 
     }
+
 
     var lowerCaseQ = confirm("Do You Want Lower Case Letters?");
     if (lowerCaseQ){
-        (lowerCaseQ);
+        console.log(lowerCaseQ);
     }
-    else {
+
+    else if (lowerCaseQ === false){
+        console.log(lowerCaseQ)
+        
+    }
+
+    else{
         return null;
     }
 
 
     var upperCaseQ = confirm("Would You Like Upper Case Letters?");
     if (upperCaseQ){
-        (upperCaseQ);
+        console.log(upperCaseQ);
     }
+
+    else if (upperCaseQ === false) {
+        console.log(upperCaseQ);
+    }
+
     else {
-        return null;
+       return null; 
     }
     
     var generatedPassword = [];
+
+
+
+
 
     
       
